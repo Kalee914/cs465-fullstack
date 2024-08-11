d2c10ea65628f3e4eab97de04dfc90317b86584b
Kalee Li <kalee914@gmail.com>
2024-08-11 16:11:03 -0400

    Module 7 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,19 +1,30 @@
 const express = require('express'); // Express App
 const router = express.Router(); // Router Logic
+const { expressjwt: jwt } = require("express-jwt");
+
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ['HS256'],
+  userProperty:"payload",
+});
 
 // import the controllers
+const authController = require("../controllers/authentication");
 const tripsController = require('../controllers/trips');
+
+router.route("/login").post(authController.login);
+router.route("/register").post(authController.register);
 
 // Define route for trips endpoint
 router
   .route("/trips")
   .get(tripsController.tripsList) // Get Method routes tripList
-  .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+  .post(auth,tripsController.tripsAddTrip); // POST Method Adds a Trip
 
 // GET Method rutes tripsFindByCode - requires parameter
 router
   .route("/trips/:tripCode")
   .get(tripsController.tripsFindByCode)
-  .put(tripsController.tripsUpdateTrip);
+  .put(auth,tripsController.tripsUpdateTrip);
 
 module.exports = router;
